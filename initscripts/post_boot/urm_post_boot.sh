cbc806ccb2e9c75d3542c91f1919c7b3352de7ae
Varun Singhal <varusing@qti.qualcomm.com>
2026-05-14 10:48:04 +0530

    Add URM post_boot scripts for QCM6490, QCM8300, QCS9075, QCS9100
    - Add urm_post_boot.sh as dispatcher based on machine name
    - Add target-specific post_boot scripts for each platform
    - Update CMakeLists.txt to install scripts to /etc/urm/initscripts/post_boot
    
    Signed-off-by: Varun Singhal <varusing@qti.qualcomm.com>

---
+++ initscripts/post_boot/urm_post_boot.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+if [ -f /sys/devices/soc0/machine ]; then
+    machine=`cat /sys/devices/soc0/machine`
+fi
+
+case "$machine" in
+    QCM6490 | qcm6490)
+        /etc/urm/initscripts/post_boot/post_boot_qcm6490.sh
+        ;;
+    QCM8300 | qcm8300)
+        /etc/urm/initscripts/post_boot/post_boot_qcm8300.sh
+        ;;
+    QCS9100 | qcs9100)
+        /etc/urm/initscripts/post_boot/post_boot_qcs9100.sh
+        ;;
+    QCS9075 | qcs9075)
+        /etc/urm/initscripts/post_boot/post_boot_qcs9075.sh
+        ;;
+    *)
+        # Empty default case
+        ;;
+esac
